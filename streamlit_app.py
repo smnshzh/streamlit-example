837eda24bc0e9facdb0bd97f82ced3a397ecb893
smnshzh <smnshzh@gmail.com>
2023-09-28 14:40:07 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import extra_streamlit_components as stx
+from geopy import Nominatim
 from streamlit.components.v1 import html
 
 # Define a function to load and run JavaScript code
@@ -31,3 +33,41 @@ if st.button("Get My Location"):
           """
     
     st.markdown(js_code, unsafe_allow_html=True)   
+    
+    
+
+
+@st.cache_resource(hash_funcs={"_thread.RlLock":lambda _:None})
+def init_rout():
+    page = {   
+            "/home":home,
+            "/outside":outside
+            }
+
+
+    return stx.Router(page)
+
+def home():
+    
+    return st.write("this is home")
+
+def outside():
+    return st.write("this is outside")
+
+router = init_rout()
+
+st.sidebar.title("Navigation")
+selected_route = st.sidebar.radio("Go to", ["home", "outside"])
+# Create a top navigation menu
+
+
+if selected_route == "home":
+    home()
+
+elif selected_route == "outside":
+    outside()
+
+
+    
+    
+
