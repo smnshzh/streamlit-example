b546cb61ae9c8fd3ea555dfe7c89b1a6391ae57d
smnshzh <smnshzh@gmail.com>
2023-10-10 19:47:31 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,73 +1,28 @@
 import streamlit as st
-import extra_streamlit_components as stx
-from geopy import Nominatim
-from streamlit.components.v1 import html
+import pandas as pd
 
-# Define a function to load and run JavaScript code
+# Create an empty DataFrame to store the data
+data = pd.DataFrame(columns=["Campaign Name", "Start Date", "End Date", "Budget", "Target Audience"])
 
+# Streamlit app header
+st.title("Marketing Data Collection App")
 
-# Create a button to request location
-if st.button("Get My Location"):
-    
-    
-    # Run the JavaScript code
-    def run_js_code(js_code):
-        html(js_code)
-    js_code = """
+# Create a form for data input
+st.subheader("Enter Campaign Details:")
+campaign_name = st.text_input("Campaign Name")
+start_date = st.date_input("Start Date")
+end_date = st.date_input("End Date")
+budget = st.number_input("Budget ($)")
+target_audience = st.text_input("Target Audience")
 
-    <script>
-    if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-    document.getElementById("demo").innerHTML =
-    "Geolocation is not supported by this browser.";
-    }
+# Add data to the DataFrame when the user clicks the "Submit" button
+if st.button("Submit"):
+    data = data.append({"Campaign Name": campaign_name, "Start Date": start_date, "End Date": end_date,
+                        "Budget": budget, "Target Audience": target_audience}, ignore_index=True)
 
-    function showPosition(position) {
-    alert('posiosion geted')
-    document.getElementById("demo").innerHTML =
-    'Latitude: ' + position.coords.latitude + '<br>' +
-    'Longitude: ' + position.coords.longitude;
-    }
-    </script>
-          """
-    
-    st.markdown(js_code, unsafe_allow_html=True)   
-    
-    
+# Display the collected data
+st.subheader("Collected Data:")
+st.write(data)
 
-
-@st.cache_resource(hash_funcs={"_thread.RlLock":lambda _:None})
-def init_rout():
-    page = {   
-            "/home":home,
-            "/outside":outside
-            }
-
-
-    return stx.Router(page)
-
-def home():
-    
-    return st.write("this is home")
-
-def outside():
-    return st.write("this is outside")
-
-router = init_rout()
-
-st.sidebar.title("Navigation")
-selected_route = st.sidebar.radio("Go to", ["home", "outside"])
-# Create a top navigation menu
-
-
-if selected_route == "home":
-    home()
-
-elif selected_route == "outside":
-    outside()
-
-
-    
-    
-
+# Optionally, you can add data visualization and analysis here.
+# For example, you can create charts using libraries like Matplotlib or Plotly.
